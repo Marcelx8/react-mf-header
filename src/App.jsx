aff783770372808862b30a4b63fabd9141b775ae
Marcel Steenkamp <marcel@yellowtail.co.za>
2021-10-27 13:07:37 +0200

    test header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Header from "./Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import "./index.css";
 const App = () => (
